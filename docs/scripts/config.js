fff1ec016567737407544f22f344e4cf3b2204ae
Agnes Corpuz <catherine_agnes.corpuz@genesys.om>
2023-06-13 13:38:27 +0800

    config

---
+++ docs/scripts/config.js
@@ -0,0 +1,14 @@
+export default {
+    // Token Implicit Grant Client ID
+    clientID: '5f3e661d-61be-4a13-b536-3f54f24e26c9',
+
+    redirectUri: 'https://agnescorpuz.github.io/chat-translator-blueprint/',
+
+    genesysCloud: {
+        // Genesys Cloud region
+        // eg. 'mypurecloud.ie', 'euw2.pure.cloud', etc...
+        region: 'mypurecloud.com'
+    },
+
+    translateServiceURI: 'https://agnescorpuz.github.io/chat-translator-blueprint/translate'
+}
